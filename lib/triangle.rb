806953236fd801f502e4c3b49d2e28133640ad8f
Cedric David <cedric.david@btopenworld.com>
2017-03-10 10:39:55 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,22 +1,33 @@
 class Triangle
   attr_accessor :sides
-  def initialize(side1, side2, side3)
-    @sides = [side1, side2, side3]
+
+  def initialize(side_a, side_b, side_c)
+    @sides = [side_a, side_b, side_c]
   end
+
+  def is_triangle?
+    if (@sides[0] + @sides[1] <= @sides[2] || @sides[1] + @sides[2] <= @sides[0] || @sides[0] + @sides[2] <= @sides[1])
+      false
+    elsif @sides.any? { |side| side == nil || side <= 0}
+      false
+    else
+      true
+    end
+  end
+
   def kind
-    if (@sides[0]== nil || @sides[1] == nil || @sides[2] == nil ) ||  (@sides[0] <= 0|| @sides[1] <= 0 || @sides[2] <= 0) || (@sides[0] + @sides[1] <= @sides[2] || @sides[1] + @sides[2] <= @sides[0] || @sides[0] + @sides[2] <=  @sides[1])
+    if !is_triangle?
       raise TriangleError
-    elsif @sides[0] == @sides[1] && @sides[1] == @sides[2]
+    elsif sides.uniq.count == 1
       :equilateral
-    elsif @sides[0] == @sides[1] || @sides[0] == @sides[2] || @sides[1] == @sides[2]
+    elsif sides.uniq.count == 2
       :isosceles
     else
       :scalene
     end
   end
-
-
 end
-class TriangleError < StandardError
 
-end
+  class TriangleError < StandardError
+    # triangle error code
+  end
