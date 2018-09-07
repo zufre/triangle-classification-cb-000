69030459f6e89c46327d797cbdab283a047cf240
Cedric David <cedric.david@btopenworld.com>
2018-09-07 11:33:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,6 +28,6 @@ class Triangle
 end
 class TriangleError < StandardError
   def message
-      "you must give the get_married method an argument of an instance of the person class!"
+    
   end
 end
