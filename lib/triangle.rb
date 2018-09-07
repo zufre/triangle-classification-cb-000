1693e2565b01ba84b37a1bae8782121fc93ae627
Cedric David <cedric.david@btopenworld.com>
2018-09-07 11:35:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,8 +10,6 @@ class Triangle
     if @side1 == 0 || @side2 == 0 || @side3 == 0 || @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
       begin
         raise TriangleError
-      
-
       end
     elsif @side1 == @side2 && @side2 == @side3
       :equilateral
